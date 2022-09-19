96c7ddf6ec00152c4763a05231a4fef80b562c34
nikulin_sergei <nikulin2403@gmail.com>
2022-09-19 03:54:04 +0300

    task 1-4

---
+++ unit_one/task1.py
@@ -0,0 +1,27 @@
+#todo: Определить в коде  переменные:
+
+#1. Целочисленного типа
+#2. Вещественного типа
+#3. Логического типа
+#4. Строкового типа
+#5. Пустого типа
+
+#Вывести все их типы.
+
+a = 4
+print ("a =", a)
+print (type (a))
+b = 8.9
+print ("b = ", b)
+print (type(b))
+c = True
+print ("c =", c)
+print (type(c))
+d = "Good evening"
+print ("d =", d)
+print (type(d))
+e = None
+print ("e =", e)
+print (type(e))
+
+
---
+++ unit_one/task4.py
@@ -0,0 +1,10 @@
+# todo:  Создайте переменные, что бы вывести требуемый текст.
+
+# задан нерабочий код
+# требуемый вывод:
+# Мой стек: python, javascript, php
+programming_lang_1 = "python,"
+programming_lang_2 = "javascript,"
+programming_lang_3 = "php"
+print("Мой стек:", programming_lang_1, programming_lang_2, programming_lang_3)
+
